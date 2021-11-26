ba93d4c754844c18f3d8ed528d4aa0aab0d7a9e4
Sara Alexie Noomi Nederskov <6583335@rts-365.dk>
2021-11-26 12:39:02 +0100

    made navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,22 @@ import Image from 'next/image'
 
 const Navbar = () => {
     return (
-        <nav>
-            <div className='logo'>
+        <nav className="md:max-w-screen-lg md:justify-end md:items-end md:flex-row mx-auto flex border-b border-gray-400">
+            <div className='mr-auto'>
                 <Link href="/">
                     <a>
                         <Image src='/images/logo.png' height={80} width={80} />
                     </a>
                 </Link>
             </div>
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/about"><a>About</a></Link>
-            <Link href="/cats"><a>Cat List</a></Link>
+            <Link href="/"><a className="mr-4 self-end">Home</a></Link>
+            <Link href="/about"><a className="mr-4 self-end">About</a></Link>
+            <Link href="/cats"><a className="mr-4 self-end">Cat List</a></Link>
+            {/*<div className="md:hidden self-end block absolute top-5 right-5">
+                <div className="w-6 h-1 mb-1 bg-black"></div>
+                <div className="w-6 h-1 mb-1 bg-black"></div>
+                <div className="w-6 h-1 mb-1 bg-black"></div>
+                </div>*/}
         </nav>
     );
 }
