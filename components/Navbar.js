ddb593759a5f90ac742c9ce68649f112108111d5
Sara Alexie Noomi Nederskov <6583335@rts-365.dk>
2021-11-26 13:07:24 +0100

    made site responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Navbar = () => {
     return (
-        <nav className="md:max-w-screen-lg md:justify-end md:items-end md:flex-row mx-auto flex border-b border-gray-400">
+        <nav className="md:max-w-screen-lg md:justify-end md:items-end md:flex-row mx-auto top-5 left-5 flex border-b border-gray-400">
             <div className='mr-auto'>
                 <Link href="/">
                     <a>
@@ -11,14 +11,14 @@ const Navbar = () => {
                     </a>
                 </Link>
             </div>
-            <Link href="/"><a className="mr-4 self-end">Home</a></Link>
-            <Link href="/about"><a className="mr-4 self-end">About</a></Link>
-            <Link href="/cats"><a className="mr-4 self-end">Cat List</a></Link>
+            <Link href="/"><a className="md:mr-9 mr-4 self-end">Home</a></Link>
+            <Link href="/about"><a className="md:mr-9 mr-4 self-end">About</a></Link>
+            <Link href="/cats"><a className="md:mr-9 mr-4 self-end">Cat List</a></Link>
             {/*<div className="md:hidden self-end block absolute top-5 right-5">
                 <div className="w-6 h-1 mb-1 bg-black"></div>
                 <div className="w-6 h-1 mb-1 bg-black"></div>
                 <div className="w-6 h-1 mb-1 bg-black"></div>
-                </div>*/}
+            </div>*/}
         </nav>
     );
 }
--- pages/about.js
+++ pages/about.js
@@ -8,11 +8,11 @@ const About = () => {
                 <title>Next Project | About</title>
             </Head>
             <h1 className="text-center">This is an about-page</h1>
-            <h3 className="mb-4">
+            <h3 className="mb-4 p-2">
                 Pretend this is all about cats 😅 😂 🤣
             </h3>
-            <article className="flex">
-                <p className="w-2/5 mr-4">
+            <article className="md:flex">
+                <p className="md:w-2/5 md:mr-4 p-2">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Maiores facilis nisi dolores vero? Labore ipsa veniam quisquam perspiciatis autem doloremque 
                     repellat maxime nam magnam eaque, id fugit? Dolore quod sequi fugit, 
@@ -22,12 +22,15 @@ const About = () => {
                     repellat maxime nam magnam eaque, id fugit? Dolore quod sequi fugit, 
                     impedit harum in earum ducimus vel, labore, accusantium magnam.
                 </p>
-                <Image
-                    src="/images/walkcat.jpeg"
-                    width={577}
-                    height={433}
-                    className="shadow-xl rounded"
-                />
+                <div className="text-center">
+                    <Image
+                        src="/images/walkcat.jpeg"
+                        width={577}
+                        height={433}
+                        className="shadow-xl rounded"
+                    />
+                </div>
+                
             </article>
         </section>
     );
