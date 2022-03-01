3344bcf9bb481816e6e9206472564a9a54fd4d69
Sara Alexie Noomi Nederskov <6583335@rts-365.dk>
2022-03-01 08:50:00 +0100

    fixed apos

--- pages/about.js
+++ pages/about.js
@@ -17,18 +17,19 @@ const About = () => {
                     when vacuum cleaner is on scratch strangers and poo on
                     owners food. Throwup on your pillow catty ipsum. Eat a rug
                     and furry furry hairs everywhere oh no human coming lie on
-                    counter don't get off counter making bread on the bathrobe
-                    bleghbleghvomit my furball really tie the room together.
-                    Grab pompom in mouth and put in water dish. Reaches under
-                    door into adjacent room more napping, more napping all the
-                    napping is exhausting, for my water bowl is clean and
-                    freshly replenished, so i'll drink from the toilet pee in
-                    the shoe and in the middle of the night i crawl onto your
-                    chest and purr gently to help you sleep or love to play with
-                    owner's hair tie. Ooh, are those your $250 dollar sandals?
-                    lemme use that as my litter box. Hiding behind the couch
-                    until lured out by a feathery toy pose purrfectly to show my
-                    beauty destroy house in 5 seconds. Sniff sniff.
+                    counter don`&apos;`t get off counter making bread on the
+                    bathrobe bleghbleghvomit my furball really tie the room
+                    together. Grab pompom in mouth and put in water dish.
+                    Reaches under door into adjacent room more napping, more
+                    napping all the napping is exhausting, for my water bowl is
+                    clean and freshly replenished, so i`&apos;`ll drink from the
+                    toilet pee in the shoe and in the middle of the night i
+                    crawl onto your chest and purr gently to help you sleep or
+                    love to play with owner`&apos;`s hair tie. Ooh, are those
+                    your $250 dollar sandals? lemme use that as my litter box.
+                    Hiding behind the couch until lured out by a feathery toy
+                    pose purrfectly to show my beauty destroy house in 5
+                    seconds. Sniff sniff.
                 </p>
                 <div className="text-center">
                     <Image
